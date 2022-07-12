5fa729739eecd64a1029d4dcda7a263bd980a886
redam90 <100067170+redam90@users.noreply.github.com>
2022-07-12 22:46:24 +0200

    .

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed:1000,
+        navText:["prev","next"],
+        responsive:{
+            0:{
+               nav:false,
+            },
+            768:{
+                nav:true
+            }
+        }
+    })
+});
+
+$('#project-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:24,
+    smartSpeed:1000,
+    navText:["NEXT","PREV"],
+    responsive:{
+        0:{
+          items:1,
+          nav:false,
+          margin:0,
+        },
+        768:{
+           items:2,
+        },
+        1140:{
+            items:2,
+            center:true,
+            dots:true,
+        }
+
+    }
+});
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        dots:true,
+        margin:10,
+        smartSpeed:800,
+        navText:["NEXT","PREV"],
+        responsive:{
+            0:{
+             
+            },
+            768:{
+                items:1,
+             },
+            },
+            1140:{
+                items:1,
+            }
+    
+        });
